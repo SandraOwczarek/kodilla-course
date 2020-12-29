1a9a6cae46f7e7ba77890538398b7bea68b639b8
imię i nazwisko <sandraowczarek5@gmail.com>
2020-12-29 23:02:41 +0100

    Zadanie 8.2

--- kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/test/FirstChallenge.java
@@ -1,2 +1,26 @@
-package com.kodilla.exception.test;public class FirstChallenge {
+package com.kodilla.exception.test;
+
+public class FirstChallenge {
+    public double divide(double a, double b) throws ArithmeticException {
+        if (b == 0) {
+            throw new ArithmeticException();
+        }
+        return a / b;
+    }
+    /**
+     * This main can throw an ArithmeticException!!!
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        FirstChallenge firstChallenge = new FirstChallenge();
+        try {
+            Double result = firstChallenge.divide(3, 0);
+            System.out.println(result);
+        } catch (ArithmeticException e) {
+            System.out.println("You can't devide with 0");
+        } finally {
+            System.out.println("but I will always be here for You ");
+        }
+    }
 }
