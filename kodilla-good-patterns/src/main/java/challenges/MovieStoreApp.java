eadd4588fd068e6033ccbc8db28c482eb0cbe53d
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-13 19:38:20 +0100

    funkcyjna iteracja po tytulach

--- kodilla-good-patterns/src/main/java/challenges/MovieStoreApp.java
+++ kodilla-good-patterns/src/main/java/challenges/MovieStoreApp.java
@@ -1,2 +1,13 @@
-package challenges;public class MovieStoreApp {
+package challenges;
+import java.util.stream.Collectors;
+
+public class MovieStoreApp {
+
+    public static void main(String[] args) {
+        MovieStore movieStore = new MovieStore();
+        String stringOfBooks = movieStore.getMovies().values().stream()
+                .flatMap(s -> s.stream())
+                .collect(Collectors.joining("!"));
+        System.out.println(stringOfBooks);
+    }
 }
