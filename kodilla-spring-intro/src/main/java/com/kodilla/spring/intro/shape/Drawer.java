03b6b605efe33b932dc39a60d28f69bab49d9fa8
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-17 22:19:54 +0100

    Let's start again- modul 14

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,2 +1,14 @@
-package com.kodilla.spring.intro.shape;public class Drawer {
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+
+    public void doDrawing() {
+        Shape shape;
+
+        shape = new Circle();
+        shape.draw();
+
+        shape = new Triangle();
+        shape.draw();
+    }
 }
