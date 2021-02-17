08b7f55d5d512d67e36a8d18463fee33e82341c7
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-17 19:05:29 +0100

    14.1 Bean User

--- kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
+++ kodilla-spring-intro/src/main/java/com/kodilla/spring/intro/shape/Drawer.java
@@ -1,2 +1,14 @@
-package com.kodilla.spring.intro.shape;public class Drawer {
+package com.kodilla.spring.intro.shape;
+
+package com.kodilla.spring.intro.shape;
+
+public class Drawer {
+
+    public void doDrawing() {
+        Circle circle = new Circle();
+        Triangle triangle = new Triangle();
+
+        circle.draw();
+        triangle.draw();
+    }
 }
