64a2648084c1a9cbf39a716df65ef21ed34aea0d
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-18 16:15:02 +0100

    14.3 Tablica z listami zadan

--- kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/portfolio/BoardConfig.java
@@ -1,2 +1,18 @@
-package com.kodilla.spring.portfolio;public class BoardConfig {
+package com.kodilla.spring.portfolio;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
+
+@Configuration
+public class BoardConfig {
+    @Bean
+    public Board getBoard(){
+        return new Board(getTaskList(),getTaskList(),getTaskList());
+    }
+    @Bean
+    @Scope("prototype")
+    public TaskList getTaskList() {
+        return new TaskList();
+    }
 }
--- kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/portfolio/BoardTestSuite.java
@@ -1,2 +1,29 @@
-package com.kodilla.spring.portfolio;public class BoardTestSuite {
+package com.kodilla.spring.portfolio;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+@SpringBootTest
+public class BoardTestSuite {
+
+    @Test
+    void testTaskAdd(){
+        //Given
+        ApplicationContext context = new AnnotationConfigApplicationContext(BoardConfig.class);
+        Board board = context.getBean(Board.class);
+        //When
+        String taskToDo = "Buy groceries";
+        String taskInProgress = "Coking Dinner";
+        String taskDone = "Clean after dinner";
+        board.getToDoList().addTask(taskToDo);
+        board.getInProgressList().addTask(taskInProgress);
+        board.getDoneList().addTask(taskDone);
+        //Then
+        Assertions.assertEquals("Buy groceries", board.getToDoList().getTasks().get(0));
+        Assertions.assertEquals("Coking Dinner", board.getInProgressList().getTasks().get(0));
+        Assertions.assertEquals("Clean after dinner", board.getDoneList().getTasks().get(0));
+    }
 }
