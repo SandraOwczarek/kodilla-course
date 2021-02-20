8e98cc8b59a43fc1ea1dec37b357738732e85b7f
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-20 19:13:08 +0100

    15.1 Zadanie Loggi

--- kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -1,2 +1,18 @@
-package com.kodilla.patterns.singleton;public class LoggerTestSuite {
+package com.kodilla.patterns.singleton;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class LoggerTestSuite {
+    @Test
+    void shouldReturnTheLastLogWritten() {
+        //Given
+        Logger logger = Logger.INSTANCE;
+        logger.log("access password change");
+        //When
+        String log = logger.getLastLog();
+        //Then
+        assertEquals("access password change", log);
+    }
 }
