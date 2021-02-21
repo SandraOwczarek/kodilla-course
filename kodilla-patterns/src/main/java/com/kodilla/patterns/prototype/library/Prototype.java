b431da20db4e4d92b8d35e24103b08bc27281812
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-21 15:44:16 +0100

    Zadanie 15.2 Klonujemy biblioteke

--- kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Prototype.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/prototype/library/Prototype.java
@@ -1,2 +1,9 @@
-package com.kodilla.patterns.prototype.library;public class Prototype {
+package com.kodilla.patterns.prototype.library;
+
+public class Prototype<T> implements Cloneable {
+
+    @Override
+    public T clone() throws CloneNotSupportedException {
+        return (T)super.clone();
+    }
 }
--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/library/LibraryTestSuite.java
@@ -1,2 +1,55 @@
-package com.kodilla.patterns.prototype.library;public class LibraryTestSuite {
+package com.kodilla.patterns.prototype.library;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.IntStream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+
+public class LibraryTestSuite {
+
+    @Test
+    void testGetBooks(){
+
+        //given
+        Library library = new Library("First Library");
+        library.setBooks(createBookSet());
+
+        Library shallowClonedLibrary = null;
+        try {
+            shallowClonedLibrary = library.shallowCopy();
+        } catch (CloneNotSupportedException e){
+            System.out.println(e.getMessage());
+        }
+
+        Library deepClonedLibrary = null;
+        try {
+            deepClonedLibrary = library.deepCopy();
+        } catch (CloneNotSupportedException e){
+            System.out.println(e.getMessage());
+        }
+
+        //when
+        Book book = new Book("title 1", "author 1", LocalDate.of(2012,3,22));
+        library.getBooks().remove(book);
+
+        //then
+        assertEquals(4, library.getBooks().size());
+        assertEquals(4, shallowClonedLibrary.getBooks().size());
+        assertEquals(library.getBooks(), shallowClonedLibrary.getBooks());
+        assertNotEquals(library.getBooks(), deepClonedLibrary.getBooks());
+    }
+
+    private Set<Book> createBookSet(){
+        Set<Book> books = new HashSet<>();
+        IntStream.iterate(1, n -> n + 1)
+                .limit(5)
+                .forEach(n -> books.add(new Book("title " + n, "author " + n,
+                        LocalDate.now().minusYears(10-n).plusMonths(n).plusDays(n))));
+        return books;
+    }
 }
