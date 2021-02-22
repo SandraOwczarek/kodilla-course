f5ac92fdff392ffb829b3439b5792999f9464eef
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-22 15:45:52 +0100

    Zadanie 13.5 Wyzwanie-Loty

--- kodilla-good-patterns/src/main/java/challenges/flights/Application.java
+++ kodilla-good-patterns/src/main/java/challenges/flights/Application.java
@@ -1,2 +1,10 @@
-package challenges.flights;public class Application {
+package challenges.flights;
+
+public class Application {
+    public static void main(String[] args) {
+        FlightScanner flightScanner = new FlightScanner();
+        flightScanner.lookForDepartureAirport("WMI");
+        flightScanner.lookForArrivalAirport("WRO");
+        flightScanner.lookForConnectingAirport("WAW", "WRO");
+    }
 }
--- kodilla-good-patterns/src/main/java/challenges/flights/FlightDatabase.java
+++ kodilla-good-patterns/src/main/java/challenges/flights/FlightDatabase.java
@@ -1,2 +1,22 @@
-package challenges.flights;public class FlightDatabase {
+package challenges.flights;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class FlightDatabase {
+    private Set<Flight> flightSet = new HashSet<>();
+
+    public Set<Flight> getFlightSet() {
+        flightSet.add(new Flight("WAW", "WRO"));
+        flightSet.add(new Flight("WRO", "POZ"));
+        flightSet.add(new Flight("SZZ", "WRO"));
+        flightSet.add(new Flight("WMI", "SZZ"));
+        flightSet.add(new Flight("KRK", "WAW"));
+        flightSet.add(new Flight("WRO", "RZE"));
+        flightSet.add(new Flight("RZE", "SZZ"));
+        flightSet.add(new Flight("WMI", "KRK"));
+        flightSet.add(new Flight("KRK", "LCJ"));
+        flightSet.add(new Flight("GDN", "WMI"));
+        return flightSet;
+    }
 }
