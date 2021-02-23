2a0523c9fd1d71728c0e0f8d269fe23c872538d9
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-02-23 20:06:33 +0100

    Zadanie 15.5 Big Mac

--- kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -1,4 +1,27 @@
 package com.kodilla.patterns.builder.bigmac;
 
+import org.junit.Assert;
+import org.junit.Test;
+
 public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew(){
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .bun("Sesam Seed bun")
+                .burgers(3)
+                .ingredient("iceberg lettuce")
+                .sauce("Thousand Island Dressing")
+                .ingredient("becon")
+                .ingredient("pickles")
+                .ingredient("onion")
+                .build();
+
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+
+    }
 }
