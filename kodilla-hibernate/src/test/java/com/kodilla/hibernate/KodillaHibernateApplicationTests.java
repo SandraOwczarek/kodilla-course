b63fb932013db27ab315719f4110bb20c6868148
Sandra Owczarek <sandraowczarek5@gmail.com>
2021-03-07 16:41:21 +0100

    update 17.2

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/KodillaHibernateApplicationTests.java
@@ -1,13 +1,2 @@
-package com.kodilla.hibernate;
-
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
-
-@SpringBootTest
-class KodillaHibernateApplicationTests {
-
-    @Test
-    void contextLoads() {
-    }
-
+public class KodillaHibernateApplicationTests {
 }
